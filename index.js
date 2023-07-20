b3f237bb5f8485d72ce0eb0905c632d262aa3929
nitesh sinwar <w3docs_email@example.com>
2023-07-20 16:54:56 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+//calculate the total price of the order
+total = 0;
+for (i = 0; i < 10; i++) {
+    total += i;
+}
+print("Total price is: " + total);
+print("Total price is: " + total);
+// Path: index.js
+console.log("Total price is: " + total);
+console.log("Total price is: " + total);
