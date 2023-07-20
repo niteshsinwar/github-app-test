3429bb5e5b1db1ecfa3984cc7ad756912e0c8288
nitesh sinwar <w3docs_email@example.com>
2023-07-20 17:03:02 +0530

    test commit

--- index.js
+++ index.js
@@ -4,7 +4,4 @@ for (i = 0; i < 10; i++) {
     total += i;
 }
 print("Total price is: " + total);
-print("Total price is: " + total);
-// Path: index.js
-console.log("Total price is: " + total);
 console.log("Total price is: " + total);
